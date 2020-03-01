370f3eddad84fbdf280765f374750c472c6d8c07
grenader <igor.kanshyn@gmail.com>
2020-03-01 15:24:25 -0500

    updated 'ops' scripts to specify all variable parts as parameters

--- ops/pull.sh
+++ ops/pull.sh
@@ -1,7 +1,12 @@
-echo "Puling and deploying a new image of MediaWiki PHP side"
+echo "Pulling and deploying a new image of MediaWiki PHP side"
 echo "Note, that it requires 'docker login' and previous image should be stopped."
 
-echo "Tag: $1"
+[[ -z "$1" ]] && { echo "Parameter "image tag" (1) is empty" ; exit 1; }
+[[ -z "$2" ]] && { echo "Parameter "new container name" (2) is empty" ; exit 2; }
+
+read -p "Make sure you have stopped your current mediawiki image. Press enter to continue or cancel the script ..."
+
+echo "Tag: $1, new contater name: $2"
 
 docker pull grenader/mw-mediawiki:$1
 
@@ -16,5 +21,5 @@ imageId=$(echo "${BASH_REMATCH[1]}" | sed 's/ .*//' | xargs)
 echo "-------------------------------"
 echo "Downloaded imageId: $imageId"
 
-docker run --name new3mw1 --link mysql56 -p 80:80 -e MEDIAWIKI_SITE_SERVER=http://wikivedas.com  -e MEDIAWIKI_ADMIN_PASS=11 -e MEDIAWIKI_DB_PASSWORD=11 -e MEDIAWIKI_DB_NAME=mw_db3 -e MEDIAWIKI_DB_USER=root  -e MEDIAWIKI_DB_HOST=mysql56 -d $imageId
+docker run --name $2 --link mysql56 -p 80:80 -e MEDIAWIKI_SITE_SERVER=http://wikivedas.com  -e MEDIAWIKI_ADMIN_PASS=11 -e MEDIAWIKI_DB_PASSWORD=11 -e MEDIAWIKI_DB_NAME=mw_db3 -e MEDIAWIKI_DB_USER=root -e MEDIAWIKI_DB_HOST=mysql56 -d $imageId
 
--- ops/push.sh
+++ ops/push.sh
@@ -1,5 +1,8 @@
 echo "Pushing a new image of MediaWiki PHP side"
 echo "Note, that it requires 'docker login'"
+
+[[ -z "$1" ]] && { echo "Parameter "container name``" (1) is empty" ; exit 1; }
+
 OUTPUT="$(date +"%Y%b%d%H")"
 #echo "$OUTPUT" | tr '[:upper:]' '[:lower:]'
 repoName=`echo "$OUTPUT" | tr '[:upper:]' '[:lower:]'`
@@ -7,6 +10,6 @@ repoName=`echo "$OUTPUT" | tr '[:upper:]' '[:lower:]'`
 echo RepoName: ${repoName}
 echo Tag: ${OUTPUT}
 
-docker commit new3mw3 mw-mediawiki-${repoName}
+docker commit $1 mw-mediawiki-${repoName}
 docker tag mw-mediawiki-${repoName} grenader/mw-mediawiki:${OUTPUT}
 docker push  grenader/mw-mediawiki:${OUTPUT}
